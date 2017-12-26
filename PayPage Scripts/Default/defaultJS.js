2aa60c1be112a3d62efa17a037262a3e7f02d88d
jwinchell35 <34691447+jwinchell35@users.noreply.github.com>
2017-12-26 14:42:14 -0600

    Updates
    Updates

--- PayPage Scripts/Default/defaultJS.js
+++ PayPage Scripts/Default/defaultJS.js
@@ -37,6 +37,7 @@ function resetBtn_Click()
 	document.getElementById('user_data_1').value = '';
 	document.getElementById('declinedSP').innerHTML = '';
 
+	document.getElementById('user_data_1').style.display = 'none'
 	document.getElementById('miscMessageRow').style.display = 'none';
 	document.getElementById('miscMessageSpacerRow').style.display = 'none';
 	document.getElementById('first_name_msg').style.display = 'none';
@@ -86,7 +87,8 @@ function initPage()
 		of their choosing.  Define user data labels here.
 	*/
 	document.getElementById('userData1Lbl').value = 'User Data 1:';
-	
+	document.getElementById('user_data_1').style.display = 'none'
+
 	document.getElementById('creditCardLbl').value = 'Card Number:';
 	document.getElementById('expDateLbl').value = 'Expiration Date:';
 	document.getElementById('cvv2Lbl').value = 'CVV Value:';
@@ -102,6 +104,11 @@ function initPage()
 	amt = document.getElementById('amount');
 	amt.readOnly = 'readonly';
 	amt.tabIndex = -1;
+	if(document.getElementById('tran_code').value = 'Sales')
+	{
+		document.getElementById('amount').style.border = '0';
+            document.getElementById('amountRow').style.display = 'inline';
+	}
 	
 	document.getElementById('invoiceNumLbl').value = 'Invoice Number:';
 	document.getElementById('radioLbl').value = 'Transaction Type';
